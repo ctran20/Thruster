67ef0eb2f0bc79fd1776b1049ec07a6f53444ac4
Cat Tran <54409102+ctran20@users.noreply.github.com>
2020-02-15 13:25:32 -0500

    Created Rocket

---
+++ Project Thruster/Assets/Scripts/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(0, 20, 0);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+
+        }
+    }
+}
