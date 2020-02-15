a7ffae5da92746d5aa24a7bffac0e1005508ef52
Cat Tran <54409102+ctran20@users.noreply.github.com>
2020-02-15 18:17:06 -0500

    Polished Rocket

--- Project Thruster/Assets/Scripts/Rocket.cs
+++ Project Thruster/Assets/Scripts/Rocket.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     private Rigidbody rb;
+    private AudioSource auds;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
+        auds = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -23,16 +25,24 @@ public class Rocket : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rb.AddForce(0, 20, 0);
+            rb.AddRelativeForce(0, 10, 0);
+            if (!auds.isPlaying)
+            {
+                auds.Play();
+            }
+        }
+        else
+        {
+            auds.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
